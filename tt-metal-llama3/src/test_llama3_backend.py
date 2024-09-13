40ba661bea67ca3ff223bcd603c2e7c868a77d5e
Tom Stesco <tstesco@tenstorrent.com>
2024-09-13 00:53:01 -0400

    improve logging for test_llama3_backend.py

--- tt-metal-llama3/src/test_llama3_backend.py
+++ tt-metal-llama3/src/test_llama3_backend.py
@@ -1,7 +1,8 @@
 import queue
 import os
-from pathlib import Path
+import logging
 import time
+from pathlib import Path
 from unittest.mock import Mock, patch
 
 import torch
@@ -12,11 +13,8 @@ from inference_logger import get_logger
 logger = get_logger(__name__)
 logger.info(f"importing {__name__}")
 
-test_prompts_outputs = [
-    ("This is a test prompt.", "this is test output, much longer now"),
-    ("Another prompt.", "also test output"),
-]
-
+backend_logger = logging.getLogger("llama3_backend")
+backend_logger.setLevel(logging.DEBUG)
 
 def test_llama3_backend():
     prompt_q = queue.Queue()
@@ -24,13 +22,13 @@ def test_llama3_backend():
     status_q = queue.Queue()
 
     # user_id, prompt, params
-    default_params, _ = get_user_parameters({"max_tokens": 64})
+    default_params, _ = get_user_parameters({"max_tokens": 128})
     rag_context = ""
     prompt_q.put(
         ("INIT_ID-1", "How do you get to Carnegie Hall?", rag_context, default_params)
     )
     prompt_q.put(("INIT_ID-2", "Another prompt", rag_context, default_params))
-    run_backend(prompt_q, output_q, status_q, verbose=False, loop_once=True)
+    run_backend(prompt_q, output_q, status_q, verbose=True, loop_once=True)
     logger.info("finished")
 
 
