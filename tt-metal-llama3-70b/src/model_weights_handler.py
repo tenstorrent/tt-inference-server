31738b4ac203bead7e8e453527d1e5ddf1dd3d7e
Tom Stesco <tstesco@tenstorrent.com>
2024-09-24 20:17:11 +0000

    adding compatability env vars for LLAMA3_* required by tt-metal llama_common.py

--- tt-metal-llama3-70b/src/model_weights_handler.py
+++ tt-metal-llama3-70b/src/model_weights_handler.py
@@ -2,6 +2,7 @@
 #
 # SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 
+import os
 from pathlib import Path
 
 from inference_config import inference_config
@@ -34,15 +35,22 @@ def get_model_weights_and_tt_cache_paths():
     logger.info(f"MODEL_WEIGHTS_PATH:={weights_path}")
     available_weights = [item.name for item in model_weights_dir_path.iterdir()]
     if not weights_id and not weights_path:
-        return default_weights_handler(
+        weights_path, tt_cache_path = default_weights_handler(
             weights_id, model_weights_dir_path, tt_cache_dir_path
         )
     elif weights_id and weights_path:
-        return custome_weights_handler(weights_id, weights_path, tt_cache_dir_path)
+        weights_path, tt_cache_path = custome_weights_handler(weights_id, weights_path, tt_cache_dir_path)
     elif (weights_id and not weights_path) or (not weights_id and weights_path):
         err_msg = f"Must set both MODEL_WEIGHTS_ID and MODEL_WEIGHTS_PATH. weights_path=:{weights_path}, but weights_id:={weights_id}. Available_weights:={available_weights}"
         logger.error(err_msg)
         raise ValueError(err_msg)
+
+    # llama3 specific
+    # note: tt-metal/models/demos/t3000/llama2_70b/tt/llama_common.py requires these environment variables are set
+    os.environ["LLAMA3_CKPT_DIR"]=str(weights_path)
+    os.environ["LLAMA3_TOKENIZER_PATH"]=str(Path(weights_path).joinpath("tokenizer.model"))
+    os.environ["LLAMA3_CACHE_PATH"]=str(tt_cache_path)
+    return weights_path, tt_cache_path
 
 
 def default_weights_handler(weights_id, model_weights_dir_path, tt_cache_dir_path):
