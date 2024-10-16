bbd1845cbf12bda5cd2748f6208726148d8f3fc9
Mithun Vanniasinghe <mvanniasinghe@tenstorrent.com>
2024-10-16 16:31:13 +0000

    remove max gen tokens bc unused

--- tt-metal-mistral-7b/src/mistral_7b_backend.py
+++ tt-metal-mistral-7b/src/mistral_7b_backend.py
@@ -56,7 +56,6 @@ class UserInfo:
         # then send new chars
         self.generated_tokens = []
         self.num_generated_chars = 0
-        self.max_generated_tokens = 120
 
 class PrefillDecodeBackend:
     def __init__(
@@ -105,7 +104,6 @@ class PrefillDecodeBackend:
         # embed_on_device not currently supported, set to False to run embedding layer on CPU vs on TT device 
         self.embed_on_device = False 
         self.prefill_seq_len = 0 # 0 is default if there is no prefill 
-        self.max_generated_tokens = 120
 
     def get_users(self):
         return [u for u in self.users if u]
