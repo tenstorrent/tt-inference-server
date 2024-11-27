2f82417f142cf8b6bb6d452584b667de217d1669
Tom Stesco <tstesco@tenstorrent.com>
2024-11-27 04:45:28 +0000

    adding utils/logging_utils.py to handle RawStatLogger, logging_init_wrapper, and set_vllm_logging_config

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,7 +1,15 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
 import os
-import datetime
+import sys
+from datetime import datetime
 import json
 from pathlib import Path
+import logging
+import multiprocessing
+from logging.config import dictConfig
 
 from vllm.engine.metrics_types import StatLoggerBase, Stats, SupportsMetricsInfo
 from vllm.engine.metrics import logger
@@ -17,6 +25,66 @@ def logging_init_wrapper(self, *args, **kwargs):
     num_scheduler_steps = self.scheduler_config.num_scheduler_steps
     batch_size = self.scheduler_config.max_num_seqs
     self.stat_loggers["raw_logging"] = RawStatLogger(num_scheduler_steps, batch_size)
+
+
+LOG_TIMESTAMP = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+LOG_PATH = Path(os.getenv("CACHE_ROOT", ".")) / "logs" / f"vllm_{LOG_TIMESTAMP}.log"
+
+
+def get_logging_dict(log_path, level="DEBUG"):
+    logging_dict = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "vllm": {
+                "class": "vllm.logging.NewLineFormatter",
+                "format": "%(levelname)s %(asctime)s %(filename)s:%(lineno)d] %(message)s",
+                "datefmt": "%m-%d %H:%M:%S",
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "vllm",
+                "level": level,
+                "stream": "ext://sys.stdout",
+            },
+            "file": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "formatter": "vllm",
+                "maxBytes": 104857600,  # 100MB
+                "backupCount": 5,
+                "level": level,
+                "filename": str(log_path),
+                "mode": "a",
+            },
+        },
+        "loggers": {
+            "vllm": {
+                "handlers": ["console", "file"],
+                "level": level,
+                "propagate": False,
+            }
+        },
+    }
+    return logging_dict
+
+
+def write_logging_config(logging_dict, log_dir):
+    config_path = log_dir / "vllm_logging_config.json"
+    with open(config_path, "w") as file:
+        json.dump(logging_dict, file, indent=4)
+
+    return config_path
+
+
+def set_vllm_logging_config(level="INFO"):
+    LOG_PATH.parent.mkdir(exist_ok=True, parents=True)
+    logging_dict = get_logging_dict(LOG_PATH, level)
+    config_path = write_logging_config(logging_dict, LOG_PATH.parent)
+    # need to apply the logging config to the root logger
+    logging.config.dictConfig(logging_dict)
+    return config_path, LOG_PATH
 
 
 class RawStatLogger(StatLoggerBase):
