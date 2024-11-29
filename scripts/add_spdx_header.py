9b35e17da6d8c7f7b890ac3ec5a99c28fdf432f9
Tom Stesco <tstesco@tenstorrent.com>
2024-11-29 12:39:21 -0500

    add pyproject.toml for ruff linter, update README.md link to LLaMa 3.1 70B, add new top level dirs to add_spdx_header.py (#46)

--- scripts/add_spdx_header.py
+++ scripts/add_spdx_header.py
@@ -16,6 +16,7 @@ SPDX_HEADER = """# SPDX-License-Identifier: Apache-2.0
 
 SPDX_DATE = str(current_year) + " Tenstorrent AI ULC\n"
 
+
 def add_spdx_header(file_path):
     with open(file_path, "r+") as file:
         content = file.read()
@@ -23,16 +24,24 @@ def add_spdx_header(file_path):
             file.seek(0, 0)
             file.write(SPDX_HEADER + SPDX_DATE + "\n" + content)
 
+
 if __name__ == "__main__":
     # List of directories to process here
     repo_root = Path(__file__).resolve().parent.parent
     directories_to_process = [
         repo_root / "tt-metal-llama3-70b",
+        repo_root / "vllm-tt-metal-llama3-70b",
         repo_root / "tt-metal-mistral-7b",
+        repo_root / "tests",
+        repo_root / "utils",
+        repo_root / "evals",
+        repo_root / "benchmarking",
     ]
     # Walk through the specified directories and add the header to all relevant files
     for directory in directories_to_process:
         for file_path in directory.rglob("*"):
             # Check if the file is Python, Dockerfile, or Bash
-            if file_path.suffix in (".py", ".sh") or file_path.name.endswith("Dockerfile"):
+            if file_path.suffix in (".py", ".sh") or file_path.name.endswith(
+                "Dockerfile"
+            ):
                 add_spdx_header(file_path)
