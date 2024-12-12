b71bbe22c01465d7577f89e9f9f66ba3b26461b2
bgoelTT <bgoel@tenstorrent.com>
2024-12-12 12:22:35 -0500

    Remove test image from commit history, pull from internet at runtime

--- tt-metal-yolov4/tests/locustfile.py
+++ tt-metal-yolov4/tests/locustfile.py
@@ -3,11 +3,14 @@
 # SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 
 import io
+import requests
 from PIL import Image
 from locust import HttpUser, task
 
 # Save image as JPEG in-memory for load testing
-pil_image = Image.open("test_image.jpg")
+# Load sample image
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+pil_image = Image.open(requests.get(url, stream=True).raw)
 pil_image = pil_image.resize((320, 320))  # Resize to target dimensions
 buf = io.BytesIO()
 pil_image.save(
