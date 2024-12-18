50e6ef72a733986fc306609e6246dfa96e846a1d
Tom Stesco <tstesco@tenstorrent.com>
2024-12-18 05:13:08 +0000

    update benchmark sweeps

--- benchmarking/prompt_client_online_benchmark.py
+++ benchmarking/prompt_client_online_benchmark.py
@@ -147,11 +147,18 @@ def run_sequence_length_test(
 
 if __name__ == "__main__":
     combinations = [
-        # {"input_len": 128, "output_len": 128, "batch_size": 1, "num_prompts": 32},
-        {"input_len": 128, "output_len": 1024, "batch_size": 1, "num_prompts": 16},
-        {"input_len": 2048, "output_len": 128, "batch_size": 1, "num_prompts": 32},
-        {"input_len": 128, "output_len": 4096, "batch_size": 1, "num_prompts": 8},
-        {"input_len": 2048, "output_len": 2048, "batch_size": 1, "num_prompts": 8},
+        # sweeps for batch-1
+        {"input_len": 128, "output_len": 128, "batch_size": 1, "num_prompts": 16},
+        {"input_len": 128, "output_len": 1024, "batch_size": 1, "num_prompts": 8},
+        {"input_len": 2048, "output_len": 128, "batch_size": 1, "num_prompts": 8},
+        {"input_len": 128, "output_len": 4096, "batch_size": 1, "num_prompts": 4},
+        {"input_len": 2048, "output_len": 2048, "batch_size": 1, "num_prompts": 4},
+        # sweeps for batch-32
+        {"input_len": 128, "output_len": 128, "batch_size": 32, "num_prompts": 32},
+        {"input_len": 128, "output_len": 1024, "batch_size": 32, "num_prompts": 32},
+        {"input_len": 2048, "output_len": 128, "batch_size": 32, "num_prompts": 32},
+        {"input_len": 128, "output_len": 4096, "batch_size": 32, "num_prompts": 32},
+        {"input_len": 2048, "output_len": 2048, "batch_size": 32, "num_prompts": 32},
     ]
 
     # Create output directory
