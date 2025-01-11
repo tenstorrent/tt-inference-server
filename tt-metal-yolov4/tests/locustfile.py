99d6bb266533e829a9ea80d8bdbd695f439a495d
Benjamin Goel <bgoel@tenstorrent.com>
2025-01-10 19:56:37 -0500

    Add JWT API key authentication to YOLOv4 (#65)
    * Add JWT API key authentication
    
    * Add authentication header to locust testing and add API demonstration test
    
    * Add invalid API key test

--- tt-metal-yolov4/tests/locustfile.py
+++ tt-metal-yolov4/tests/locustfile.py
@@ -2,26 +2,16 @@
 #
 # SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 
-import io
-import requests
-from PIL import Image
 from locust import HttpUser, task
+from utils import get_auth_header, sample_file
 
-# Save image as JPEG in-memory for load testing
-# Load sample image
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-pil_image = Image.open(requests.get(url, stream=True).raw)
-pil_image = pil_image.resize((320, 320))  # Resize to target dimensions
-buf = io.BytesIO()
-pil_image.save(
-    buf,
-    format="JPEG",
-)
-byte_im = buf.getvalue()
-file = {"file": byte_im}
+
+# load sample file in memory
+file = sample_file()
 
 
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
-        self.client.post("/objdetection_v2", files=file)
+        headers = get_auth_header()
+        self.client.post("/objdetection_v2", files=file, headers=headers)
