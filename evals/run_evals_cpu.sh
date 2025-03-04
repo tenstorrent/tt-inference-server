ad2bbe4ca323cd66a66d82b891f5859448e73b0b
Tom Stesco <tstesco@tenstorrent.com>
2025-03-04 05:56:56 +0000

    add CPU eval instructions and run script

---
+++ evals/run_evals_cpu.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+#
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
+original_dir=$PWD
+
+if [[ -z "${HF_MODEL_REPO_ID}" ]]; then
+    echo "⛔ Error: env var HF_MODEL_REPO_ID is not set. This must be the model HF repo e.g. 'meta-llama/Llama-3.3-70B-Instruct'"
+    exit 1
+fi
+
+# set up lm_eval and evals datasets
+cd $HOME
+if python3 -c "import lm_eval" 2>/dev/null; then
+    echo "lm_eval is installed."
+else
+    echo "Installing lm_eval ..."
+    pip install git+https://github.com/tstescoTT/lm-evaluation-harness.git#egg=lm-eval[ifeval,api]
+fi
+
+if [ -d "$HOME/llama-recipes" ]; then
+    echo "The directory $HOME/llama-recipes exists."
+else
+    echo "The directory ~/llama-recipes does not exist."
+    git clone https://github.com/tstescoTT/llama-recipes.git $HOME/llama-recipes
+    cd $HOME/llama-recipes/tools/benchmarks/llm_eval_harness/meta_eval
+    python3 prepare_meta_eval.py --config_path ./eval_config.yaml
+    mkdir -p $HOME/lm-evaluation-harness
+    cp -rf work_dir/ $HOME/lm-evaluation-harness/
+fi
+
+# run evals
+export OPENAI_API_KEY=$(python3 -c 'import os; import json; import jwt; json_payload = json.loads("{\"team_id\": \"tenstorrent\", \"token_id\": \"debug-test\"}"); encoded_jwt = jwt.encode(json_payload, os.environ["JWT_SECRET"], algorithm="HS256"); print(encoded_jwt)')
+cd $HOME/lm-evaluation-harness/
+
+# GPQA
+lm_eval \
+--model local-completions \
+--model_args model=${HF_MODEL_REPO_ID},base_url=http://127.0.0.1:7000/v1/completions,num_concurrent=6,max_retries=4,tokenized_requests=False,add_bos_token=True,timeout=999999 \
+--gen_kwargs model=${HF_MODEL_REPO_ID},stop="<|eot_id|>",stream=False \
+--tasks meta_gpqa \
+--batch_size auto \
+--output_path ${CACHE_ROOT}/eval_output \
+--include_path ./work_dir \
+--seed 42  \
+--log_samples
+
+# IFEval
+lm_eval \
+--model local-completions \
+--model_args model=${HF_MODEL_REPO_ID},base_url=http://127.0.0.1:7000/v1/completions,num_concurrent=6,max_retries=4,tokenized_requests=False,add_bos_token=True,timeout=999999 \
+--gen_kwargs model=${HF_MODEL_REPO_ID},stop="<|eot_id|>",stream=False \
+--tasks meta_ifeval \
+--batch_size auto \
+--output_path ${CACHE_ROOT}/eval_output \
+--include_path ./work_dir \
+--seed 42  \
+--log_samples
+
+cd $original_dir
