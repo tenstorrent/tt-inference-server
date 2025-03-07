e785c326017187ea4635bdd1d6f64962a6edb780
stisi <stisi@tenstorrent.com>
2025-03-07 20:10:23 +0000

    Import fixes

--- tests/tools/__init__.py
+++ tests/tools/__init__.py
@@ -2,3 +2,9 @@
 #
 # SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
+from .local_server import start_server
+from .single_test import single_benchmark_execution
+from .multi_test import mass_benchmark_execution
+from .run_benchmark import original_run_benchmark
+
+__all__ = ["start_server", "single_benchmark_execution", "mass_benchmark_execution", "original_run_benchmark"]
