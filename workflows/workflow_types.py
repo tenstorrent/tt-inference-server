b4fbeee36bc430f308604ae784acbb56d9d39c22
stisi <stisi@tenstorrent.com>
2025-03-26 18:41:39 +0000

    Workflow Config and JWT Setup. Successful first CLI run

--- workflows/workflow_types.py
+++ workflows/workflow_types.py
@@ -22,6 +22,7 @@ class WorkflowType(IntEnum):
 
 
 class WorkflowVenvType(IntEnum):
+    TESTS_RUN_SCRIPT = auto()
     EVALS_RUN_SCRIPT = auto()
     BENCHMARKS_RUN_SCRIPT = auto()
     REPORTS_RUN_SCRIPT = auto()
