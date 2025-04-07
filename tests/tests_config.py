252217774d216ed9021cdea626eb1701e113548e
stisi <stisi@tenstorrent.com>
2025-04-07 19:34:14 +0000

    Num Prompts fix

--- tests/tests_config.py
+++ tests/tests_config.py
@@ -12,7 +12,7 @@ class TestsConfig:
         self.device = device
         self.param_space = TestParamSpace(self.hf_model_repo, self.device)
 
-class TestParamSpace: # TODO: Hard coded values are arbitrary
+class TestParamSpace: # Note: Hard coded values are arbitrary
     def __init__(self, model_name, device):
         self.max_context_length = self.trim_max_context(model_name, device)
         self.max_seq_values = [self.max_context_length, 1312]
@@ -21,7 +21,7 @@ class TestParamSpace: # TODO: Hard coded values are arbitrary
         self.output_size_values = [128, 256]
         self.max_concurrent_value = MODEL_CONFIGS[model_name].max_concurrency_map[DeviceTypes.from_string(device)]
         self.max_concurrent_values = [2, self.max_concurrent_value]
-        self.num_prompts_values = [2, self.max_concurrent_values]
+        self.num_prompts_values = [2, self.max_concurrent_value]
 
     def trim_max_context(self, model_name, device):
         model_config = MODEL_CONFIGS[model_name]
