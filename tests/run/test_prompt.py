cff2e72ba49df524a0c3e6d95ee30d589a8d160e
stisi <stisi@tenstorrent.com>
2025-04-07 20:13:24 +0000

    Remove old commented approach

--- tests/run/test_prompt.py
+++ tests/run/test_prompt.py
@@ -10,9 +10,6 @@ class TestPrompt:
         self.prompt = self.generate_prompt(test_params, mode)
 
     def generate_prompt(self, token_parameters, mode):
-        # it = {"input_len": int(hyperparam['continuous_batch'] / hyperparam['max_concurrent'] - value), "output_len": value,
-        #       "max_concurrent": hyperparam['max_concurrent'], "num_prompts": hyperparam['num_prompts']}
-        # TODO: Explore the above and if dispersing max context length across batch or num_prompts is appropriate for tests
 
         it = {"input_len": int(token_parameters["input_size"]), "output_len": token_parameters["output_size"],
               "max_concurrent": token_parameters['max_concurrent'], "num_prompts": token_parameters['num_prompts']}
