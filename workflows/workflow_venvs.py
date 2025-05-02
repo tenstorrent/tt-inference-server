7f6b08eccc08a98b62ae89bc2c2821dc29fd68f2
Tom Stesco <tstesco@tenstorrent.com>
2025-05-02 15:39:55 +0000

    add reasoning from samt/venv_for_reason

--- workflows/workflow_venvs.py
+++ workflows/workflow_venvs.py
@@ -81,6 +81,19 @@ def setup_evals(
     return True
 
 
+def setup_evals_reason(
+    venv_config: VenvConfig,
+    model_config: "ModelConfig",  # noqa: F821
+    uv_exec: Path,
+) -> bool:
+    logger.warning("this might take 5 to 15+ minutes to install on first run ...")
+    run_command(
+        f"{uv_exec} pip install --python {venv_config.venv_python} git+https://github.com/tstescoTT/lm-evaluation-harness.git@db1c714579c982a1b35d6bbdcca875d402614fb9#egg=lm-eval[api,r1_evals] pyjwt==2.7.0 pillow==11.1",
+        logger=logger,
+    )
+    return True
+
+
 def setup_evals_meta(
     venv_config: VenvConfig,
     model_config: "ModelConfig",  # noqa: F821
@@ -286,6 +299,9 @@ _venv_config_list = [
     VenvConfig(venv_type=WorkflowVenvType.EVALS_META, setup_function=setup_evals_meta),
     VenvConfig(
         venv_type=WorkflowVenvType.EVALS_VISION, setup_function=setup_evals_vision
+    ),
+    VenvConfig(
+        venv_type=WorkflowVenvType.EVALS_REASON, setup_function=setup_evals_reason
     ),
     VenvConfig(
         venv_type=WorkflowVenvType.BENCHMARKS_HTTP_CLIENT_VLLM_API,
