ee71b5bdcf6ff543a7e2bf0d194c48123e746b1b
stisi <stisi@tenstorrent.com>
2025-06-17 18:36:59 +0000

    Deleting old files for Refactoring name of Model Readiness Tests to Spec_Tests

--- tests/run/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-#
-# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
-
-from .tests import Tests
-from .tests_env_vars import TestsEnvVars
-from .test_run import TestRun
-from .test_prompt import TestPrompt
