386246f3bd4ef4dd19047edd1b18570f8ddf01f8
stisi <stisi@tenstorrent.com>
2025-07-28 20:22:59 -0400

    Commit updates and benchmark target updates

--- workflows/model_config.py
+++ workflows/model_config.py
@@ -461,7 +461,7 @@ config_templates = [
         status=ModelStatusTypes.EXPERIMENTAL,
     ),
     ModelConfigTemplate(
-        impl=llama3_impl,
+        impl=tt_transformers_impl,
         device_model_spec_map={
             DeviceTypes.N300: DeviceModelSpec(
                 max_concurrency=32,
@@ -475,8 +475,8 @@ config_templates = [
             ),
         },
         weights=["Qwen/Qwen2.5-7B", "Qwen/Qwen2.5-7B-Instruct"],
-        tt_metal_commit="v0.56.0-rc33",
-        vllm_commit="e2e0002ac7dc",
+        tt_metal_commit="v0.61.0",
+        vllm_commit="0c0ee9e",
         status=ModelStatusTypes.EXPERIMENTAL,
     ),
     ModelConfigTemplate(
