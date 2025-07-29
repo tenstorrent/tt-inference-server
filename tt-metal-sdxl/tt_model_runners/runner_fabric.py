f00ac57eb17539a7b697d36a7595f449eeaa34c4
Igor Djuric <idjuric@tenstorrent.com>
2025-07-29 12:48:19 +0200

    SD 3.5 Large support & mock mode & threads instead of queues

--- tt-metal-sdxl/tt_model_runners/runner_fabric.py
+++ tt-metal-sdxl/tt_model_runners/runner_fabric.py
@@ -5,12 +5,19 @@
 
 from config.settings import settings
 from tt_model_runners.base_device_runner import DeviceRunner
+from tt_model_runners.mock_runner import MockRunner
 
 
-def get_device_runner() -> DeviceRunner:
+def get_device_runner(worker_id: str) -> DeviceRunner:
         model_runner = settings.model_runner
         if model_runner == "tt-sdxl":
             from tt_model_runners.sdxl_runner import TTSDXLRunner
-            return TTSDXLRunner()
+            return TTSDXLRunner(worker_id)
+        elif model_runner == "tt-sd3.5":
+            from tt_model_runners.sd35_runner import TTSD35Runner
+            return TTSD35Runner(worker_id)
+        elif model_runner == "mock":
+            from tt_model_runners.mock_runner import MockRunner
+            return MockRunner(worker_id)
         else:
             raise ValueError(f"Unsupported model runner: {model_runner}")
