d2acc45d56175d3e83500eb7892c55003f429ceb
Igor Djuric <idjuric@tenstorrent.com>
2025-08-12 10:28:58 +0200

    Recover dead workers (#469)
    * Recover dead workers
    
    * Deep reset and improvements
    
    * Scheduler changes
    
    * More error handling

--- tt-metal-sdxl/model_services/image_service.py
+++ tt-metal-sdxl/model_services/image_service.py
@@ -47,6 +47,13 @@ class ImageService(BaseModel):
             'runner_in_use': settings.model_runner,
         }
 
+    async def deep_reset(self) -> bool:
+        """Reset the device and all the scheduler workers and processes"""
+        self.logger.info("Resetting device")
+        # Create a task to run in the background
+        asyncio.create_task(self.scheduler.deep_restart_workers())
+        return True
+
     @log_execution_time("Starting workers")
     def startWorkers(self):
         self.scheduler.startWorkers()
