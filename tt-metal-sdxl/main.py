bff39cd939dc9227bd5d6aa19a668fe6290ecc76
Adam Roberge <aroberge@tenstorrent.com>
2025-08-19 16:37:29 -0400

    reset settings to default inference run to sdxl

--- tt-metal-sdxl/main.py
+++ tt-metal-sdxl/main.py
@@ -37,7 +37,3 @@ app.include_router(api_router)
 
 # Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8050)
