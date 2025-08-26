6144642e6f6f86934cf07c5d571ed5beebf24481
Aleksandar Cvejic <acvejic@tenstorrent.com>
2025-08-26 17:58:18 +0200

    fix

--- tt-metal-sdxl/forge_entrypoint.sh
+++ tt-metal-sdxl/forge_entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -euo pipefail
-echo "Reseting environment..."
+echo "Reseting environment variables..."
 unset TT_METAL_HOME
 unset PYTHONPATH
 unset WH_ARCH_YAML
 unset ARCH_NAME
+echo "Environment variables reset."
 exec "$@"
