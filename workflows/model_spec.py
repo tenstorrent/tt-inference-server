9556557e1355e18e0a89e36f0281147574650a7a
Radoica Draskic <rdraskic@tenstorrent.com>
2025-08-27 09:57:30 +0000

    Reduce max context

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1069,7 +1069,7 @@ spec_templates = [
             DeviceModelSpec(
                 device=DeviceTypes.N150,
                 max_concurrency=32,
-                max_context=64 * 1024,
+                max_context=32 * 1024,
                 default_impl=True,
             ),
             DeviceModelSpec(
