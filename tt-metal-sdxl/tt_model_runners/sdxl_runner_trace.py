956bbcccb09285fa19408f4858f184aacf999fe6
Igor Djuric <idjuric@tenstorrent.com>
2025-09-02 06:16:35 +0000

    Use is_galaxy for SDXL class init

--- tt-metal-sdxl/tt_model_runners/sdxl_runner_trace.py
+++ tt-metal-sdxl/tt_model_runners/sdxl_runner_trace.py
@@ -102,6 +102,7 @@ class TTSDXLRunnerTrace(BaseDeviceRunner):
                 torch_pipeline=self.pipeline,
                 pipeline_config=TtSDXLPipelineConfig(
                     encoders_on_device=True,
+                    is_galaxy=self.settings.use_graph_device_split,
                     num_inference_steps=self.settings.num_inference_steps,
                     guidance_scale=5.0,
                 ),
