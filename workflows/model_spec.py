57e9e9fa64af7063f2766f19e2131c2e818b3b7b
Radoica Draskic <rdraskic@tenstorrent.com>
2025-09-02 15:33:11 +0000

    Update comment

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1069,7 +1069,7 @@ spec_templates = [
             DeviceModelSpec(
                 device=DeviceTypes.N150,
                 max_concurrency=32,
-                max_context=8 * 1024, # To be updated once https://github.com/tenstorrent/tt-metal/issues/23854 is resolved
+                max_context=8 * 1024, # revert back to 64 * 1024 once https://github.com/tenstorrent/tt-metal/issues/23854#issuecomment-3245384170 is resolved
                 default_impl=True,
             ),
             DeviceModelSpec(
