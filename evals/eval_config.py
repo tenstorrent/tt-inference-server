d89f58b8c8cd4409696809e30ac2bf27ac1371e5
stisi <stisi@tenstorrent.com>
2025-09-05 14:48:39 +0000

    Commenting out LiveCodeBench due to indefinite hangs

--- evals/eval_config.py
+++ evals/eval_config.py
@@ -1175,28 +1175,28 @@ _eval_config_list = [
                     "stream": "false",
                 },
             ),
-            EvalTask(
-                task_name="livecodebench",
-                workflow_venv_type=WorkflowVenvType.EVALS      ,
-                score=EvalTaskScore(
-                    published_score=31.4,
-                    published_score_ref="https://qwenlm.github.io/blog/qwen2.5-coder-family/",
-                    gpu_reference_score=42.46,
-                    gpu_reference_score_ref="A100 GPU benchmark results",
-                    score_func=score_task_single_key,
-                    score_func_kwargs={
-                        "result_keys": [
-                            "acc",
-                        ],
-                        "unit": "percent",
-                    },
-                ),
-                apply_chat_template=True,
-                model_kwargs={
-                    "timeout": "9999",
-                },
-                batch_size=16,
-            ),
+            # EvalTask(
+            #     task_name="livecodebench",
+            #     workflow_venv_type=WorkflowVenvType.EVALS      ,
+            #     score=EvalTaskScore(
+            #         published_score=31.4,
+            #         published_score_ref="https://qwenlm.github.io/blog/qwen2.5-coder-family/",
+            #         gpu_reference_score=42.46,
+            #         gpu_reference_score_ref="A100 GPU benchmark results",
+            #         score_func=score_task_single_key,
+            #         score_func_kwargs={
+            #             "result_keys": [
+            #                 "acc",
+            #             ],
+            #             "unit": "percent",
+            #         },
+            #     ),
+            #     apply_chat_template=True,
+            #     model_kwargs={
+            #         "timeout": "9999",
+            #     },
+            #     batch_size=16,
+            # ),
         ],
     ),
 ]
