a601cff4903b312385550ed22a0eacf7f64a0f61
Nikola Ostojic <nostojic@tenstorrent.com>
2025-09-09 10:21:35 +0200

    Update model_spec.py - max_concurrency set to 32 * 16

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1206,7 +1206,7 @@ spec_templates = [
         device_model_specs=[
             DeviceModelSpec(
                 device=DeviceTypes.GALAXY,
-                max_concurrency=32 * 4,
+                max_concurrency=32 * 16,
                 max_context=64 * 1024,
                 default_impl=True,
                 override_tt_config={
