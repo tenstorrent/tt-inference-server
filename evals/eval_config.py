972e6f211f52d6422ebffd26643eb7544c4f5003
Aleksandar Cvejic <acvejic@tenstorrent.com>
2025-09-29 01:27:11 +0200

    add eval config

--- evals/eval_config.py
+++ evals/eval_config.py
@@ -1299,6 +1299,23 @@ _eval_config_list = [
         ],
     ),
     EvalConfig(
+        hf_model_repo="stabilityai/stable-diffusion-3.5-large",
+        tasks=[
+            EvalTask(
+                task_name="load_image",
+                workflow_venv_type=WorkflowVenvType.EVALS_META,
+                include_path="work_dir",
+                max_concurrent=None,
+                apply_chat_template=False,
+                score=EvalTaskScore(
+                    published_score=15.0,
+                    published_score_ref="",
+                    score_func=lambda results: 0.0,
+                ),
+            ),
+        ],
+    ),
+    EvalConfig(
         hf_model_repo="Qwen/Qwen2.5-Coder-32B-Instruct",
         tasks=[
             EvalTask(
