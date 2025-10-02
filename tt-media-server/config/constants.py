9b96c5dda2576a93cd4efcbdcbacba87fd364307
Zeljana Torlak <ztorlak@tenstorrent.com>
2025-10-02 12:41:41 +0200

    adding brackets to device ids

--- tt-media-server/config/constants.py
+++ tt-media-server/config/constants.py
@@ -36,7 +36,7 @@ ModelConfigs = {
         "model_service": ModelServices.IMAGE.value,
         "device_mesh_shape": (1, 1),
         "is_galaxy": False,
-        "device_ids": "0",
+        "device_ids": "(0)",
         "batch_size": 1,
     },
     (SupportedModels.STABLE_DIFFUSION_XL_BASE, DeviceTypes.N300): {
@@ -44,7 +44,7 @@ ModelConfigs = {
         "model_service": ModelServices.IMAGE.value,
         "device_mesh_shape": (1, 1),
         "is_galaxy": False,
-        "device_ids": "0,1",
+        "device_ids": "(0),(1)",
         "batch_size": 2,
     },
     (SupportedModels.STABLE_DIFFUSION_XL_BASE, DeviceTypes.GALAXY): {
@@ -52,7 +52,7 @@ ModelConfigs = {
         "model_service": ModelServices.IMAGE.value,
         "device_mesh_shape": (1, 1),
         "is_galaxy": True,
-        "device_ids": "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15",
+        "device_ids": "(0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15)",
         "batch_size": 1,
     },
     (SupportedModels.STABLE_DIFFUSION_XL_BASE, DeviceTypes.T3K): {
@@ -60,13 +60,12 @@ ModelConfigs = {
         "model_service": ModelServices.IMAGE.value,
         "device_mesh_shape": (1, 1),
         "is_galaxy": False,
-        "device_ids": "0,1,2,3",
+        "device_ids": "(0),(1),(2),(3)",
         "batch_size": 2,
     },    
     (SupportedModels.STABLE_DIFFUSION_3_5_LARGE, DeviceTypes.T3K): {
         "model_runner": ModelRunners.TT_SD3_5.value,
         "model_service": ModelServices.IMAGE.value,
-        "device_ids": "", # enforce no device split, we need the whole machine
         "device_mesh_shape": (2, 4),
         "is_galaxy": False,
         "device_ids": "", #HACK to use all devices. device id split will retun and empty string to be passed to os.environ[TT_VISIBLE_DEVICES] in device_worker.py
@@ -75,9 +74,8 @@ ModelConfigs = {
     (SupportedModels.STABLE_DIFFUSION_3_5_LARGE, DeviceTypes.GALAXY): {
         "model_runner": ModelRunners.TT_SD3_5.value,
         "model_service": ModelServices.IMAGE.value,
-        "device_ids": "", # enforce no device split, we need the whole machine
         "device_mesh_shape": (4, 8),
-        "is_galaxy": False,
+        "is_galaxy": True,
         "device_ids": "", #HACK to use all devices. device id split will retun and empty string to be passed to os.environ[TT_VISIBLE_DEVICES] in device_worker.py
         "batch_size": 1
     },
@@ -86,41 +84,41 @@ ModelConfigs = {
         "model_service": ModelServices.AUDIO.value,
         "is_galaxy": False,
         "device_mesh_shape": (1, 1),
-        "device_ids": "0",
+        "device_ids": "(0)",
     },
     (SupportedModels.DISTIL_WHISPER_LARGE_V3, DeviceTypes.N300): {
         "model_runner": ModelRunners.TT_WHISPER.value,
         "model_service": ModelServices.AUDIO.value,
         "device_mesh_shape": (1, 1),
         "is_galaxy": False,
-        "device_ids": "0,1",
+        "device_ids": "(0),(1)",
     },
     (SupportedModels.DISTIL_WHISPER_LARGE_V3, DeviceTypes.GALAXY): {
         "model_runner": ModelRunners.TT_WHISPER.value,
         "model_service": ModelServices.AUDIO.value,
         "device_mesh_shape": (1, 1),
         "is_galaxy": True,
-        "device_ids": "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15",
+        "device_ids": "(0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15)",
     },
     (SupportedModels.DISTIL_WHISPER_LARGE_V3, DeviceTypes.T3K): {
         "model_runner": ModelRunners.TT_WHISPER.value,
         "model_service": ModelServices.AUDIO.value,
         "device_mesh_shape": (1, 1),
         "is_galaxy": False,
-        "device_ids": "0,1,2,3",
+        "device_ids": "(0),(1),(2),(3)",
     },
     (SupportedModels.MICROSOFT_RESNET_50, DeviceTypes.N150): {
         "model_runner": ModelRunners.FORGE.value,
         "model_service": ModelServices.CNN.value,
         "is_galaxy": False,
         "device_mesh_shape": (1, 1),
-        "device_ids": "0",
+        "device_ids": "(0)",
     },
     (SupportedModels.MICROSOFT_RESNET_50, DeviceTypes.N300): {
         "model_runner": ModelRunners.FORGE.value,
         "model_service": ModelServices.CNN.value,
         "is_galaxy": False,
         "device_mesh_shape": (1, 1),
-        "device_ids": "0,1",
+        "device_ids": "(0),(1)",
     },
 }
