e85fbff6e305b2a9e3052d372b96781ccc8a4a97
Adam Roberge <aroberge@tenstorrent.com>
2025-10-23 11:52:37 -0400

    Increase Qwen3-8B eval timeout and adjust CI downsampling for MMLU Pro  (#943)
    * Increase timeout to 3600
    
    * Reudce CI_NIGHTLY from 0.10 to 0.05

--- evals/eval_config.py
+++ evals/eval_config.py
@@ -336,6 +336,7 @@ _eval_config_list = [
                     "base_url": "http://127.0.0.1:8000/v1/completions",
                     "tokenizer_backend": "huggingface",
                     "max_length": 65536,
+                    "timeout": "3600",
                 },
                 # gen_kwargs chosen according to https://huggingface.co/Qwen/Qwen3-8B#best-practices
                 gen_kwargs={
@@ -348,7 +349,7 @@ _eval_config_list = [
                     "top_p": 0.95,
                 },
                 limit_samples_map={
-                    EvalLimitMode.CI_NIGHTLY: 0.10,
+                    EvalLimitMode.CI_NIGHTLY: 0.05,
                     EvalLimitMode.SMOKE_TEST: 0.01,
                 },
             ),
