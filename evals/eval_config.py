be6c41e3f4e6f7112eef756f05927f2cd3f1c771
Adam Roberge <aroberge@tenstorrent.com>
2025-10-23 10:39:43 -0400

    Reudce CI_NIGHTLY from 0.07 to 0.05

--- evals/eval_config.py
+++ evals/eval_config.py
@@ -349,7 +349,7 @@ _eval_config_list = [
                     "top_p": 0.95,
                 },
                 limit_samples_map={
-                    EvalLimitMode.CI_NIGHTLY: 0.07,
+                    EvalLimitMode.CI_NIGHTLY: 0.05,
                     EvalLimitMode.SMOKE_TEST: 0.01,
                 },
             ),
