bb2add9ee7462314622fc6855a91676eac31d694
Tom Stesco <tstesco@tenstorrent.com>
2025-10-31 02:12:48 +0000

    fix test script

--- tests/models_test.sh
+++ tests/models_test.sh
@@ -5,8 +5,8 @@ HF_TOKEN=${HF_TOKEN:?}
 export AUTOMATIC_HOST_SETUP=1
 export MODEL_SOURCE=huggingface
 
-python3 run.py --model Qwen/Qwen2.5-72B-Instruct --device galaxy --workflow release --docker-server --dev-mode
+python3 run.py --model Qwen2.5-72B-Instruct --device galaxy --workflow release --docker-server --dev-mode
 python3 run.py --model Llama-3.1-8B-Instruct --device galaxy --workflow release --docker-server --dev-mode
-python3 run.py --model Qwen/Qwen3-32B --device galaxy --workflow release --docker-server --dev-mode
-python3 run.py --model Qwen/Qwen3-8B --device galaxy --workflow release --docker-server --dev-mode
+python3 run.py --model Qwen3-32B --device galaxy --workflow release --docker-server --dev-mode
+python3 run.py --model Qwen3-8B --device galaxy --workflow release --docker-server --dev-mode
 python3 run.py --model Llama-3.3-70B-Instruct --device galaxy --workflow release --docker-server --dev-mode
