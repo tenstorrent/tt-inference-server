3d509edcc6d303691da20c416860f67c77e9cc84
Adam Roberge <aroberge@tenstorrent.com>
2025-11-12 15:21:44 +0000

    Add Galaxy support

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -948,6 +948,30 @@ spec_templates = [
                     "sample_on_device_mode": "decode_only",
                 },
             ),
+            DeviceModelSpec(
+                device=DeviceTypes.GALAXY,
+                max_concurrency=32 * 4,
+                max_context=128 * 1024,
+                default_impl=True,
+                env_vars={
+                    "VLLM_USE_V1": "1",
+                    "TT_MM_THROTTLE_PERF": 5,
+                },
+                vllm_args={
+                    "limit-mm-per-prompt": json.dumps({
+                        "image": 10
+                    }),
+                    "num_scheduler_steps": 1
+                },
+                override_tt_config={
+                    "l1_small_size": 24576,
+                    "worker_l1_size": 1344544,
+                    "trace_region_size": 21448704,
+                    "fabric_config": "FABRIC_1D",
+                    "sample_on_device_mode": "decode_only",
+                    "data_parallel": 4,
+                },
+            ),
         ],
         status=ModelStatusTypes.EXPERIMENTAL,
         supported_modalities=["text", "image"],
