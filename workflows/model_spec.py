dd5bec52bb9a499dc5f0f0ff1f5b0c1f25cb6bb6
Radoica Draskic <rdraskic@tenstorrent.com>
2025-11-18 14:30:28 +0000

    Reduce mm throttle

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1719,7 +1719,7 @@ spec_templates = [
                     "sample_on_device_mode": "decode_only",
                 },
                 env_vars={
-                    "TT_MM_THROTTLE_PERF": 5,
+                    "TT_MM_THROTTLE_PERF": 3,
                 },
             ),
             DeviceModelSpec(
