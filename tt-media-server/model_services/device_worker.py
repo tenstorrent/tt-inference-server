a5c68718b98e5d50af352abc0a10b022d5e56bd1
Lana Jovanovic <ljovanovic@tenstorrent.com>
2025-11-20 16:28:24 +0000

    Minor changes

--- tt-media-server/model_services/device_worker.py
+++ tt-media-server/model_services/device_worker.py
@@ -39,13 +39,9 @@ def setup_worker_environment(worker_id: str):
         tt_metal_home = os.environ.get("TT_METAL_HOME", "")
         # make sure to not override except 1,1 and 2,1 mesh sizes
         if settings.device_mesh_shape == (1, 1):
-            mesh_desc = f"{tt_metal_home}/tt_metal/fabric/mesh_graph_descriptors/n150_mesh_graph_descriptor.textproto"
+            os.environ["TT_MESH_GRAPH_DESC_PATH"] = f"{tt_metal_home}/tt_metal/fabric/mesh_graph_descriptors/n150_mesh_graph_descriptor.textproto"
         elif settings.device_mesh_shape == (2, 1):
-            mesh_desc = f"{tt_metal_home}/tt_metal/fabric/mesh_graph_descriptors/n300_mesh_graph_descriptor.textproto"
-        else:
-            return
-        os.environ["TT_MESH_GRAPH_DESC_PATH"] = mesh_desc
-
+            os.environ["TT_MESH_GRAPH_DESC_PATH"] = f"{tt_metal_home}/tt_metal/fabric/mesh_graph_descriptors/n300_mesh_graph_descriptor.textproto"
 
 def device_worker(
     worker_id: str,
