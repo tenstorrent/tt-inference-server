497d6b6dabc674546a49ee06f75f4c98654c11dc
Zeljana Torlak <ztorlak@tenstorrent.com>
2025-11-26 09:20:49 +0000

    reverting additional params

--- tt-media-server/domain/audio_processing_request.py
+++ tt-media-server/domain/audio_processing_request.py
@@ -2,7 +2,7 @@
 #
 # SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
-from typing import Dict, List, Optional, Tuple, Union
+from typing import Dict, List, Optional, Union
 
 import numpy as np
 from config.constants import AudioResponseFormat
@@ -22,12 +22,6 @@ class AudioProcessingRequest(BaseRequest):
     perform_diarization: bool = (
         False  # Whether to perform speaker diarization during preprocessing
     )
-
-    temperatures: Optional[Union[float, Tuple[float, ...]]] = 0.0
-    compression_ratio_threshold: Optional[float] = 2.4
-    logprob_threshold: Optional[float] = -2.0
-    no_speech_threshold: Optional[float] = 0.6
-    return_timestamps: Optional[bool] = False
 
     # Private fields for internal processing
     _audio_array: Optional[np.ndarray] = None
