a2848e3f1e29685627184e872d3d64933721fc38
Ubuntu <ubuntu@UF-EV-A3-GWH02.maas>
2025-12-01 08:55:26 +0000

    Chunks size updates

--- tt-media-server/config/settings.py
+++ tt-media-server/config/settings.py
@@ -151,12 +151,12 @@ class Settings(BaseSettings):
 
     def _calculate_audio_chunk_duration(self):
         worker_count = len(self.device_ids.replace(" ", "").split("),("))
-        if worker_count >= 16:
-            self.audio_chunk_duration_seconds = 2
+        if worker_count >= 8:
+            self.audio_chunk_duration_seconds = 3
         elif worker_count >= 4:
-            self.audio_chunk_duration_seconds = 5
+            self.audio_chunk_duration_seconds = 15
         else:
-            self.audio_chunk_duration_seconds = 10
+            self.audio_chunk_duration_seconds = 30
 
     def _set_config_overrides(self, model_to_run: str, device: str):
         model_name_enum = ModelNames(model_to_run)
