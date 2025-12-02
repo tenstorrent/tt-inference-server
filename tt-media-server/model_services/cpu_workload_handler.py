5e976f3a4110ef22dc1093d0684c8b68f8d1621a
Igor Djuric <idjuric@tenstorrent.com>
2025-12-02 13:14:50 +0000

    Remove double changes

--- tt-media-server/model_services/cpu_workload_handler.py
+++ tt-media-server/model_services/cpu_workload_handler.py
@@ -27,10 +27,6 @@ def _process_worker_tasks(
     logger.info(f"{worker_name} worker {worker_id} started")
 
     setup_cpu_threading_limits("2")
-    if torch.get_num_threads() != 1:
-        torch.set_num_threads(1)
-    if torch.get_num_interop_threads() != 1:
-        torch.set_num_interop_threads(1)
 
     worker_context = None
     if worker_context_setup:
