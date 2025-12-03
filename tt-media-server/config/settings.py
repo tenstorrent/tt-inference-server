858c5ece99a8f28f07982abc1258ba3834a3d0ed
Igor Djuric <idjuric@tenstorrent.com>
2025-12-03 12:53:47 +0100

    Update tt-media-server/config/settings.py
    Co-authored-by: Zeljana Torlak <ztorlak@tenstorrent.com>

--- tt-media-server/config/settings.py
+++ tt-media-server/config/settings.py
@@ -117,23 +117,17 @@ class Settings(BaseSettings):
 
     def _set_device_pairs_overrides(self):
         if self.is_galaxy:
+            device_manager = DeviceManager()
             if self.device_mesh_shape == (1, 1) and self.use_greedy_based_allocation:
                 # use device manager to use all the available devices
-                device_manager = DeviceManager()
                 devices = device_manager.get_single_devices_from_system()
-                if devices:
-                    self.device_ids = ",".join([f"({device})" for device in devices])
             if self.device_mesh_shape == (2, 1):
                 # use device manager to pair devices
-                device_manager = DeviceManager()
-                device_pairs = device_manager.get_device_pairs_from_system()
-                if device_pairs:
-                    self.device_ids = ",".join([f"{pair}" for pair in device_pairs])
+                devices = device_manager.get_device_pairs_from_system()
             elif self.device_mesh_shape == (2, 4):
-                device_manager = DeviceManager()
-                device_groups = device_manager.get_device_groups_of_eight_from_system()
-                if device_groups:
-                    self.device_ids = ",".join([f"{group}" for group in device_groups])
+                devices = device_manager.get_device_groups_of_eight_from_system()
+            if devices:
+                self.device_ids = ",".join([f"({device})" for device in devices])
 
     def _set_throttling_overrides(self):
         if self.model_runner in [
