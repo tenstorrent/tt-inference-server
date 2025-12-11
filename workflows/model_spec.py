26128fe963c83c4ff0da345c8ae0ed3db13e736b
Petar Milojevic <pmilojevic@tenstorrent.com>
2025-12-11 18:16:20 +0000

    Changed max_concurrency back to 32

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1021,7 +1021,7 @@ spec_templates = [
             ),
             DeviceModelSpec(
                 device=DeviceTypes.GALAXY,
-                max_concurrency=32*4,
+                max_concurrency=32,
                 max_context=128 * 1024,
                 default_impl=True,
                 env_vars={
@@ -1035,10 +1035,9 @@ spec_templates = [
                 override_tt_config={
                     "l1_small_size": 24576,
                     "worker_l1_size": 1344544,
-                    "trace_region_size": 22259360,
+                    "trace_region_size": 66147328,
                     "fabric_config": "FABRIC_1D_RING",
                     "sample_on_device_mode": "decode_only",
-                    "data_parallel": 4,
                 },
             ),
         ],
