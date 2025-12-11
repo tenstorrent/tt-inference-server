292b3e8ccb5761e4567d7c63a5e2ac30feeec988
Tom Stesco <tstesco@tenstorrent.com>
2025-12-11 15:38:36 -0500

    uplift trace_region_size for gemma-3-27b-it to 41434112

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1014,7 +1014,7 @@ spec_templates = [
                 override_tt_config={
                     "l1_small_size": 24576,
                     "worker_l1_size": 1344544,
-                    "trace_region_size": 21448704,
+                    "trace_region_size": 41434112,
                     "fabric_config": "FABRIC_1D",
                     "sample_on_device_mode": "decode_only",
                 },
