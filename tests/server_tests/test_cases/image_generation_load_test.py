b447505ebf028d9280057c4196c2aaa91d3cb6cc
Filip Ivanovic <fivanovic@tenstorrent.com>
2025-12-13 17:24:48 +0100

    feat: add human readable cli formatting; other fix formattting

--- tests/server_tests/test_cases/image_generation_load_test.py
+++ tests/server_tests/test_cases/image_generation_load_test.py
@@ -93,6 +93,4 @@ class ImageGenerationLoadTest(BaseTest):
         print(
             f"🚀 Total time for {batch_size} concurrent requests: {requests_duration:.2f}s"
         )
-        print(
-            f"🚀 Avg time for {batch_size} concurrent requests: {avg_duration:.2f}s"
-        )
+        print(f"🚀 Avg time for {batch_size} concurrent requests: {avg_duration:.2f}s")
