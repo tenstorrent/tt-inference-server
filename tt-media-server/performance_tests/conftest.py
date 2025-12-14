6dfcf7e5e1d6da72f2538a01d0bba659511c45b8
Djordje Madic <dmadic@tenstorrent.com>
2025-12-14 23:15:56 +0100

    Wait for 10s for server to be ready

--- tt-media-server/performance_tests/conftest.py
+++ tt-media-server/performance_tests/conftest.py
@@ -59,8 +59,8 @@ def server_process():
     )
 
     # Wait for server to be ready
-    print("Waiting 5 seconds for server to be ready...")
-    time.sleep(5)
+    print("Waiting 10 seconds for server to be ready...")
+    time.sleep(10)
     print("Assuming server is ready!")
 
     yield process
