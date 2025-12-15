e13a0ae2b11f6030a88fec2c3a19a964509be921
Lana Jovanovic <ljovanovic@tenstorrent.com>
2025-12-15 17:55:18 +0000

    Remove Unnecessary Files

--- utils/media_clients/embedding_client.py
+++ utils/media_clients/embedding_client.py
@@ -3,10 +3,8 @@
 # SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
 # Standard library imports
-import json
 import logging
 import sys
-import time
 from pathlib import Path
 
 # Third-party imports
@@ -40,44 +38,11 @@ class EmbeddingClientStrategy(BaseMediaStrategy):
 
             logger.info(f"Runner in use: {runner_in_use}")
 
+            return True
+
         except Exception as e:
             logger.error(f"Eval execution encountered an error: {e}")
             raise
-
-        logger.info("Generating eval report...")
-        benchmark_data = {}
-        benchmark_data["model"] = self.model_spec.model_name
-        benchmark_data["device"] = self.device.name.lower()
-        benchmark_data["timestamp"] = time.strftime(
-            "%Y-%m-%d %H:%M:%S", time.localtime()
-        )
-        benchmark_data["task_type"] = "embedding"
-        benchmark_data["task_name"] = self.all_params.tasks[0].task_name
-        benchmark_data["tolerance"] = self.all_params.tasks[0].score.tolerance
-        benchmark_data["published_score"] = self.all_params.tasks[
-            0
-        ].score.published_score
-        benchmark_data["score"] = 0.0  # Placeholder for actual score
-        benchmark_data["published_score_ref"] = self.all_params.tasks[
-            0
-        ].score.published_score_ref
-
-        # Make benchmark_data is inside of list as an object
-        benchmark_data = [benchmark_data]
-
-        # Write benchmark_data to JSON file
-        eval_filename = (
-            Path(self.output_path)
-            / f"eval_{self.model_spec.model_id}"
-            / self.model_spec.hf_model_repo.replace("/", "__")
-            / f"results_{time.time()}.json"
-        )
-        # Create directory structure if it doesn't exist
-        eval_filename.parent.mkdir(parents=True, exist_ok=True)
-
-        with open(eval_filename, "w") as f:
-            json.dump(benchmark_data, f, indent=4)
-        logger.info(f"Evaluation data written to: {eval_filename}")
 
     def run_benchmark(self, attempt=0) -> list[AudioTestStatus]:
         """Run benchmarks for the model."""
