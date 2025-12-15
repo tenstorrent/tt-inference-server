fbfbb036152b3e2aee6a8e4c699d980f98fbe2be
Petar <pmilojevic@tenstorrent.com>
2025-12-15 08:43:07 +0000

    Added T3K gemma-4b DP4

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -992,6 +992,28 @@ spec_templates = [
                     "fabric_config": "FABRIC_1D",
                 },
             ),
+            DeviceModelSpec(
+                device=DeviceTypes.T3K,
+                max_concurrency=32,
+                max_context=128 * 1024,
+                default_impl=True,
+                env_vars={
+                    "VLLM_USE_V1": "1",
+                },
+                vllm_args={
+                    "limit-mm-per-prompt": json.dumps({"image": 10}),
+                    "num_scheduler_steps": 1,
+                    "data_parallel_size": 4,
+                    "disable-mm-preprocessor-cache": "True",
+                },
+                override_tt_config={
+                    "l1_small_size": 24576,
+                    "worker_l1_size": 1344544,
+                    "trace_region_size": 21448704,
+                    "fabric_config": "FABRIC_1D",
+                    "data_parallel": 4,
+                },
+            ),
         ],
         status=ModelStatusTypes.EXPERIMENTAL,
         supported_modalities=["text", "image"],
@@ -1021,11 +1043,7 @@ spec_templates = [
                 override_tt_config={
                     "l1_small_size": 24576,
                     "worker_l1_size": 1344544,
-<<<<<<< HEAD
                     "trace_region_size": 51934848,
-=======
-                    "trace_region_size": 41434112,
->>>>>>> 10660ab1 (Added fix for T3K and DP4 for TG)
                     "fabric_config": "FABRIC_1D",
                     "sample_on_device_mode": "decode_only",
                 },
