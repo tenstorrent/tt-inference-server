001ea5c2bba1f8a53cfe3432497256160b88521e
Nikola Ostojic <nostojic@tenstorrent.com>
2025-12-17 12:23:56 +0000

    decode_lny

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1769,7 +1769,7 @@ spec_templates = [
                 default_impl=True,
                 override_tt_config={
                     "trace_region_size": 50000000,
-                    "always_compat_sampling": True
+                    "sampling_on_device_mode": "decode_only"
                 },
             ),
             DeviceModelSpec(
