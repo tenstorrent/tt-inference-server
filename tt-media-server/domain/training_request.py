2e8b4958e55c6f85427977e16b00dacb2ef05b8e
Milan Cupac <mcupac@tenstorrent.com>
2026-01-14 11:48:32 +0000

    revert training_request changes (will be addressed in follow up PR)

--- tt-media-server/domain/training_request.py
+++ tt-media-server/domain/training_request.py
@@ -2,8 +2,14 @@
 #
 # SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
+from typing import Optional
+
 from domain.base_request import BaseRequest
 
 
 class TrainingRequest(BaseRequest):
+    model_id: str
+    dataset_id: str
     hyperparameters: dict
+    job_type_specific_parameters: Optional[dict] = None
+    checkpoint_config: dict
