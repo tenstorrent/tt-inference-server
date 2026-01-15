1f9a07e7d446074726a1f9fae9a877f8c488dde4
Lana Jovanovic <ljovanovic@tenstorrent.com>
2026-01-15 17:44:54 +0000

    Add Qwen3-Embedding-8B Runner

--- tt-media-server/tt_model_runners/runner_fabric.py
+++ tt-media-server/tt_model_runners/runner_fabric.py
@@ -49,11 +49,15 @@ AVAILABLE_RUNNERS = {
         "tt_model_runners.vllm_forge_runner", fromlist=["VLLMForgeRunner"]
     ).VLLMForgeRunner(wid, num_threads),
     ModelRunners.VLLMBGELargeEN_V1_5: lambda wid, num_threads: __import__(
-        "tt_model_runners.vllm_bge_large_en_runner", fromlist=["VLLMBGELargeENRunner"]
+        "tt_model_runners.vllm_embedding_runner", fromlist=["VLLMBGELargeENRunner"]
     ).VLLMBGELargeENRunner(wid, num_threads),
     ModelRunners.LLM_TEST: lambda wid, num_threads: __import__(
         "tt_model_runners.llm_test_runner", fromlist=["LLMTestRunner"]
     ).LLMTestRunner(wid, num_threads),
+    ModelRunners.VLLM_QWEN_EMBEDDING_8B: lambda wid, num_threads: __import__(
+        "tt_model_runners.vllm_embedding_runner",
+        fromlist=["VLLMQwen3Embedding8BRunner"],
+    ).VLLMQwen3Embedding8BRunner(wid, num_threads),
     ModelRunners.VLLMForge_QWEN_EMBEDDING: lambda wid, num_threads: __import__(
         "tt_model_runners.vllm_forge_qwen_embedding_runner",
         fromlist=["VLLMForgeEmbeddingQwenRunner"],
