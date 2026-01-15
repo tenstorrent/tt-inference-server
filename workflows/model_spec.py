306ccf183bf2dbc992c0481d4bcb601c0c3ba7ad
Tom Stesco <tstesco@tenstorrent.com>
2026-01-15 12:55:00 +0000

    update llama config context

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1585,7 +1585,7 @@ spec_templates = [
             DeviceModelSpec(
                 device=DeviceTypes.T3K,
                 max_concurrency=32,
-                max_context=128 * 1024,
+                max_context=32 * 1024,
                 default_impl=True,
                 override_tt_config={
                     "trace_region_size": 71045120,
