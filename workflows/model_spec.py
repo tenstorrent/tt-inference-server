f82125747f38bd6b135ba95ee02d111789d9af48
Nikola Ostojic <nostojic@tenstorrent.com>
2026-01-20 18:44:58 +0100

    fixed llama-3.2-3b

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1854,6 +1854,9 @@ spec_templates = [
                 max_concurrency=32,
                 max_context=128 * 1024,
                 default_impl=True,
+                override_tt_config={
+                    "trace_region_size": 15000000
+                },
             ),
             DeviceModelSpec(
                 device=DeviceTypes.N300,
