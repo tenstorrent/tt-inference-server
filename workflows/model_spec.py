dcd8873ea909ae176e97ca7fb280c563c6c6d84f
Nikola Ostojic <nostojic@tenstorrent.com>
2026-01-21 16:45:15 +0100

    added

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1854,9 +1854,7 @@ spec_templates = [
                 max_concurrency=32,
                 max_context=128 * 1024,
                 default_impl=True,
-                override_tt_config={
-                    "trace_region_size": 10000000
-                },
+                override_tt_config={"trace_region_size": 10000000},
             ),
             DeviceModelSpec(
                 device=DeviceTypes.N300,
