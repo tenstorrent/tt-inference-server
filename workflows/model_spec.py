f47ab4f8c2e601125e6bb19170273d2f6ff009f4
Nikola Ostojic <nostojic@tenstorrent.com>
2026-01-21 16:49:24 +0100

    Reduce trace region size for LLM's that go OOM (#1828)
    * fixed qwen2.5-vl
    
    * fixed llama-3.2-3b
    
    * added llama-70b
    
    * changed trs
    
    * added

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1152,6 +1152,9 @@ spec_templates = [
                 max_concurrency=32,
                 max_context=128 * 1024,
                 default_impl=True,
+                override_tt_config={
+                    "trace_region_size": 10000000,
+                },
             ),
             DeviceModelSpec(
                 device=DeviceTypes.T3K,
@@ -1653,7 +1656,7 @@ spec_templates = [
                 max_context=128 * 1024,
                 default_impl=True,
                 override_tt_config={
-                    "trace_region_size": 71045120,
+                    "trace_region_size": 30000000,
                 },
                 env_vars={
                     "MAX_PREFILL_CHUNK_SIZE": "32",
@@ -1858,6 +1861,7 @@ spec_templates = [
                 max_concurrency=32,
                 max_context=128 * 1024,
                 default_impl=True,
+                override_tt_config={"trace_region_size": 10000000},
             ),
             DeviceModelSpec(
                 device=DeviceTypes.N300,
