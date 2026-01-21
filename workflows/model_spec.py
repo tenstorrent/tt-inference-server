d32ffc495c22ee46cc0dcf00c15b70792e4e02b8
stisiTT <stisi@tenstorrent.com>
2026-01-21 14:33:26 +0000

    Remove `vllm_args` from T3K and GALAXY device specifications in `model_spec`

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -970,20 +970,12 @@ spec_templates = [
                 max_concurrency=1,
                 max_context=1024,
                 default_impl=False,
-                vllm_args={
-                    "max_num_seqs": "1",
-                    "num_scheduler_steps": "1",
-                },
             ),
             DeviceModelSpec(
                 device=DeviceTypes.GALAXY,
                 max_concurrency=1,
                 max_context=1024,
                 default_impl=True,
-                vllm_args={
-                    "max_num_seqs": "1",
-                    "num_scheduler_steps": "1",
-                },
                 env_vars={
                     "MESH_DEVICE": "(4, 8)",  # Override default TG->(8,4) to use (4,8) mesh grid that worked on bare metal
                 },
