0cb725595e3d6a22e9bc47d3bb5f7fcecd585639
stisiTT <stisi@tenstorrent.com>
2026-01-24 01:10:40 +0000

    Returning `MESH_DEVICE` to `env_vars` and adjust mesh grid configuration in `model_spec`.

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -981,8 +981,12 @@ spec_templates = [
                 max_context=1024,
                 default_impl=True,
                 vllm_args={
-                    "num_scheduler_steps": "1",
                     "max_num_seqs": "1",
+                    "num_scheduler_steps": "1",
+                },
+                env_vars={
+                    "VLLM_USE_V1": "1",
+                    "MESH_DEVICE": "(4, 8)",  # Override default TG->(8,4) to use (4,8) mesh grid that worked on bare metal
                 },
             ),
         ],
