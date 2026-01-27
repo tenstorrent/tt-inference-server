56b97952f789b3acacb07ea52e1757b2056726bc
Veljko Maksimovic <vmaksimovic@tenstorrent.com>
2026-01-27 14:59:35 +0100

    Change trace_region for Llama models on p150x4

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1717,7 +1717,7 @@ spec_templates = [
                 max_context=128 * 1024,
                 default_impl=True,
                 override_tt_config={
-                    "trace_region_size": 30000000,
+                    "trace_region_size": 58000000,
                 },
             ),
         ],
@@ -1981,7 +1981,7 @@ spec_templates = [
                 override_tt_config={
                     "data_parallel": 4,
                     "sample_on_device_mode": "decode_only",
-                    "trace_region_size": 33000000,
+                    "trace_region_size": 42000000,
                 },
             ),
         ],
