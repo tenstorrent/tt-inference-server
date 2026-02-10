a8b5d0ef8a761e209369431219763d8a3946c117
Tom Stesco <tstesco@tenstorrent.com>
2026-02-10 11:01:15 -0500

    for min_rab_gb assume fp16 equivalent weights, add 0.5x overhead buffer (#2072)

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -447,7 +447,8 @@ class ModelSpec:
                 )
 
         if not self.min_ram_gb and self.param_count:
-            object.__setattr__(self, "min_ram_gb", self.param_count * 4)
+            # assume fp16 equivalent weights, add 0.5x overhead buffer
+            object.__setattr__(self, "min_ram_gb", self.param_count * 2.5)
 
         # Generate default docker image if not provided
         if not self.docker_image:
