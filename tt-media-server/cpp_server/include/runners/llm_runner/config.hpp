2fd913fe017ef3410e5006ddbcde63794de4a63c
Djordje Madic <dmadic@tenstorrent.com>
2026-02-23 09:34:50 +0000

    Refactor

--- tt-media-server/cpp_server/include/runners/llm_runner/config.hpp
+++ tt-media-server/cpp_server/include/runners/llm_runner/config.hpp
@@ -11,12 +11,12 @@ enum class DeviceBackend {
 
 struct Config {
   static constexpr size_t MAX_INPUT_TOKENS = 131072;  // 128k
-  int max_num_batched_tokens = MAX_INPUT_TOKENS;
+  int max_num_batched_tokens = 64 * MAX_INPUT_TOKENS;
   static constexpr int max_num_seqs = 1; // Temporary hardcoded value for Deepseek blitz decode
   int eos = 1;
   int kvcache_block_size = 256;
   int num_kvcache_blocks = 512;
-  DeviceBackend device = DeviceBackend::Sockets;
+  DeviceBackend device = DeviceBackend::Mock;
 };
 
 }  // namespace llm_engine
