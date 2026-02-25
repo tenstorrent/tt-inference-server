770d456d2c628bd7ac0eb3696999edb34b7f0214
Igor Djuric <idjuric@tenstorrent.com>
2026-02-25 16:11:02 +0000

    Try with other requirements

--- tt-media-server/run_uvicorn.sh
+++ tt-media-server/run_uvicorn.sh
@@ -4,4 +4,8 @@
 
 #!/bin/bash
 set -eo pipefail
+
+# Suppress xFormers warnings when using CPU-only PyTorch
+export XFORMERS_FORCE_DISABLE_TRITON=1
+
 uvicorn --host 0.0.0.0 main:app --lifespan on --port 8000
