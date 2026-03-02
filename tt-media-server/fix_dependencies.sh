a288c95e95b9e4ba6027e50846abe9c5ffc6b4ed
Igor Djuric <idjuric@tenstorrent.com>
2026-03-02 07:52:42 +0000

    Fix dependencies post script

---
+++ tt-media-server/fix_dependencies.sh
@@ -0,0 +1,10 @@
+# Remove packages that might contain CUDA
+uv pip uninstall xformers diffusers torch torchvision torchaudio
+
+# Install CPU-only versions
+uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+# Install xformers withouth and CUDA sub-depts
+uv pip install xformers --no-deps
+
+uv pip install diffusers
