1b666dc8812b6d411d56946023cb6cad4dd1c588
Lana Jovanovic <ljovanovic@tenstorrent.com>
2026-03-03 14:21:35 +0000

    Fix

--- tt-media-server/cpp_server/src/runners/llm_runner/model_runner.cpp
+++ tt-media-server/cpp_server/src/runners/llm_runner/model_runner.cpp
@@ -26,8 +26,10 @@ std::unique_ptr<IModelRunner> make_mock_model_runner(const Config& config,
                                                      DecodeCallback callback);
 std::unique_ptr<IModelRunner> make_ttrun_model_runner(const Config& config,
                                                       DecodeCallback callback);
+#ifdef USE_METAL_CPP_LIB
 std::unique_ptr<IModelRunner> make_llama_model_runner(const Config& config,
                                                       DecodeCallback callback);
+#endif
 
 std::unique_ptr<IModelRunner> make_model_runner(const Config& config,
                                                 DecodeCallback callback) {
@@ -36,8 +38,10 @@ std::unique_ptr<IModelRunner> make_model_runner(const Config& config,
       return make_mock_model_runner(config, std::move(callback));
     case ModelRunnerType::TtRun:
       return make_ttrun_model_runner(config, std::move(callback));
+#ifdef USE_METAL_CPP_LIB
     case ModelRunnerType::Llama:
       return make_llama_model_runner(config, std::move(callback));
+#endif
     default:
       throw std::invalid_argument("Invalid model runner type");
   }
