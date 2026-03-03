a1c250e7a720df1e6c840c7bda7007519cb35415
Vladimir Petrovic <vpetrovic@tenstorrent.com>
2026-03-03 15:07:59 +0000

    fix(sdxl): Revert settings to default

--- tt-media-server/config/settings.py
+++ tt-media-server/config/settings.py
@@ -35,14 +35,14 @@ class Settings(BaseSettings):
     # Device settings
     device_ids: str = DeviceIds.DEVICE_IDS_32.value
     is_galaxy: bool = True  # used for graph device split and class init
-    device_mesh_shape: tuple = (2, 1)
+    device_mesh_shape: tuple = (1, 1)
     reset_device_command: str = "tt-smi -r"
     reset_device_sleep_time: float = 5.0
     allow_deep_reset: bool = False
     use_greedy_based_allocation: bool = True
 
     # Model settings
-    model_runner: str = ModelRunners.TT_SDXL_IMAGE_TO_IMAGE.value
+    model_runner: str = ModelRunners.TT_SDXL_TRACE.value
     model_service: Optional[str] = (
         None  # model_service can be deduced from model_runner using MODEL_SERVICE_RUNNER_MAP
     )
