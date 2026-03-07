5e697ee612a78ad192b6c06745940703421fe047
bgoelTT <bgoel@tenstorrent.com>
2026-03-07 23:16:02 +0000

    Remove vLLM V0 engine config

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -2109,13 +2109,11 @@ vlm_templates = [
                 max_concurrency=32,
                 max_context=128 * 1024,
                 default_impl=True,
-                vllm_args={"num_scheduler_steps": 1},
             ),
         ],
         status=ModelStatusTypes.EXPERIMENTAL,
         env_vars={
             "VLLM_ALLOW_LONG_MAX_MODEL_LEN": 1,
-            "VLLM_USE_V1": "1",
         },
         supported_modalities=["text", "image"],
     ),
