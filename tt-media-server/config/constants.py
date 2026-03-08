767246536eb65e118b7dde24b70b2b02fd633a37
bgoelTT <bgoel@tenstorrent.com>
2026-03-08 17:24:51 +0000

    Fix device_ids and add batch_size=2

--- tt-media-server/config/constants.py
+++ tt-media-server/config/constants.py
@@ -638,16 +638,17 @@ ModelConfigs = {
         "max_batch_size": 1,
     },
     (ModelRunners.TT_WHISPER, DeviceTypes.P300): {
-        "device_mesh_shape": (1, 2),
+        "device_mesh_shape": (1, 1),
         "is_galaxy": False,
         "device_ids": DeviceIds.DEVICE_IDS_ALL.value,
         "max_batch_size": 1,
     },
     (ModelRunners.TT_WHISPER, DeviceTypes.P300X2): {
-        "device_mesh_shape": (1, 4),
+        "device_mesh_shape": (1, 1),
         "is_galaxy": False,
-        "device_ids": DeviceIds.DEVICE_IDS_ALL.value,
-        "max_batch_size": 1,
+        "device_ids": DeviceIds.DEVICE_IDS_4.value,
+        "max_batch_size": 2,
+        "queue_for_multiprocessing": QueueType.BatchFifo.value,
     },
     (ModelRunners.VLLMForge_QWEN_EMBEDDING, DeviceTypes.N150): {
         "device_mesh_shape": (1, 1),
