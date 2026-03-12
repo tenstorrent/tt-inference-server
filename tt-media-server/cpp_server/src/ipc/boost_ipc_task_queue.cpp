80684301f375fe16f23335dac2df45ff013d6953
Kosta Novokmet <knovokmet@tenstorrent.com>
2026-03-12 16:20:19 +0100

    cpp server - worker process fix idle cpu burn (#2404)
    * fix
    
    * main fix

--- tt-media-server/cpp_server/src/ipc/boost_ipc_task_queue.cpp
+++ tt-media-server/cpp_server/src/ipc/boost_ipc_task_queue.cpp
@@ -49,6 +49,14 @@ llm_engine::Sequence* BoostIpcTaskQueue::try_pop() {
   return llm_engine::Sequence::deserialize(recv_stream);
 }
 
+llm_engine::Sequence* BoostIpcTaskQueue::receive() {
+  bi_ipc::message_queue::size_type recv_size = 0;
+  unsigned int priority = 0;
+  queue_->receive(recv_buffer_.data(), recv_buffer_.size(), recv_size, priority);
+  bi_ipc::ibufferstream recv_stream(recv_buffer_.data(), recv_size);
+  return llm_engine::Sequence::deserialize(recv_stream);
+}
+
 bool BoostIpcTaskQueue::empty() const {
   return queue_->get_num_msg() == 0;
 }
