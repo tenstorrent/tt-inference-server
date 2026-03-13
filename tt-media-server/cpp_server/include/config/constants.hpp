5792f7efccb22fa23f8a9752af339f126dd86752
Djordje Madic <dmadic@tenstorrent.com>
2026-03-13 13:49:31 +0100

    Try accumulating tokens

--- tt-media-server/cpp_server/include/config/constants.hpp
+++ tt-media-server/cpp_server/include/config/constants.hpp
@@ -112,7 +112,7 @@ namespace defaults {
     constexpr uint16_t SOCKET_PORT = 9000;
     constexpr const char* SCHEDULING_POLICY = "prefill_first";  // "prefill_first" or "max_occupancy"
     constexpr const char* LLM_DEVICE_BACKEND = "mock";  // "mock", "ttrun", "llama"
-    constexpr const bool ENABLE_ACCUMULATED_STREAMING = false;
+    constexpr const bool ENABLE_ACCUMULATED_STREAMING = true;
     constexpr size_t MAX_ACCUMULATED_TOKENS = 5;
 
 }
