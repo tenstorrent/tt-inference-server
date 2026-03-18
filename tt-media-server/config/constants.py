1cb1722b67e8db6cd53b2dd3556df39b4215b7fb
stisiTT <stisi@tenstorrent.com>
2026-03-18 00:41:07 +0000

    Fix whisper P300 device_ids to use DEVICE_IDS_2 instead of DEVICE_IDS_ALL

--- tt-media-server/config/constants.py
+++ tt-media-server/config/constants.py
@@ -645,7 +645,7 @@ ModelConfigs = {
     (ModelRunners.TT_WHISPER, DeviceTypes.P300): {
         "device_mesh_shape": (1, 1),
         "is_galaxy": False,
-        "device_ids": DeviceIds.DEVICE_IDS_ALL.value,
+        "device_ids": DeviceIds.DEVICE_IDS_2.value,
         "max_batch_size": 1,
     },
     (ModelRunners.TT_WHISPER, DeviceTypes.P300X2): {
