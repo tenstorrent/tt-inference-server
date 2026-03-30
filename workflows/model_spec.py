be5fbb7f4664dd42348822ae416368b84f4c3d70
ashwarypande <125573948+pandeashwary@users.noreply.github.com>
2026-03-30 23:12:37 +0200

    Update vllm_commit to new commit hash

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1580,7 +1580,7 @@ llm_templates = [
             ),
         ),
         tt_metal_commit="55fd115",
-        vllm_commit="84b4c53 ",
+        vllm_commit="aa4ae1e",
         inference_engine=InferenceEngine.VLLM.value,
         device_model_specs=[
             DeviceModelSpec(
