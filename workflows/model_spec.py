29e36638c530f96c73caac79e58fdc8901f3da5c
Veljko Maksimovic <vmaksimovic@tenstorrent.com>
2026-04-08 10:34:51 +0200

    V0.10.1 post release (#2795)
    * Update versions and tt-commit in model_spec
    
    * Updated docs
    
    * Updated release_spec
    
    * Updated release_model_spec
    
    * update docs

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -2048,8 +2048,8 @@ llm_templates = [
     ModelSpecTemplate(
         weights=["meta-llama/Llama-3.1-8B", "meta-llama/Llama-3.1-8B-Instruct"],
         impl=tt_transformers_impl,
-        tt_metal_commit="55fd115",
-        vllm_commit="aa4ae1e",
+        tt_metal_commit="555f240",
+        vllm_commit="22be241",
         inference_engine=InferenceEngine.VLLM.value,
         device_model_specs=[
             DeviceModelSpec(
