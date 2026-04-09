6d65e8a71ff5e919d6a4846cf52f4eb90769d728
Igor Djuric <idjuric@tenstorrent.com>
2026-04-09 13:07:14 +0000

    Fix loading

--- tt-media-server/cpp_server/src/utils/runner_factory.cpp
+++ tt-media-server/cpp_server/src/utils/runner_factory.cpp
@@ -5,11 +5,11 @@
 
 #include "runners/embedding_runner.hpp"
 #include "runners/llm_runner.hpp"
+#include "runners/sp_prefill_runner/sp_prefill_runner.hpp"
+#include "sp_pipeline_runner/sp_pipeline_runner_demo.hpp"
 #ifdef ENABLE_BLAZE
 #include "runners/sp_pipeline_runner/sp_pipeline_runner.hpp"
 #endif
-#include "runners/sp_prefill_runner/sp_prefill_runner.hpp"
-#include "sp_pipeline_runner/sp_pipeline_runner_demo.hpp"
 #include "utils/logger.hpp"
 
 namespace tt::utils::runner_factory {
@@ -27,24 +27,26 @@ std::unique_ptr<runners::IRunner> createRunner(
     default: {
       auto& cfg = std::get<config::LLMConfig>(config);
 
-#ifdef ENABLE_BLAZE
       if (cfg.runner_type == config::ModelRunnerType::PIPELINE ||
           cfg.runner_type == config::ModelRunnerType::MOCK_PIPELINE) {
-        TT_LOG_INFO("[RunnerFactory] Creating SP Pipeline runner");
+        TT_LOG_INFO("[RunnerFactory] Creating SP Pipeline runner (demo)");
         return std::make_unique<runners::SpPipelineRunnerDemo>(cfg, resultQueue,
                                                                taskQueue);
-      } else if (cfg.runner_type == config::ModelRunnerType::PIPELINE_MANAGER) {
-        TT_LOG_INFO("[RunnerFactory] Creating SP Pipeline runner");
+      }
+
+#ifdef ENABLE_BLAZE
+      if (cfg.runner_type == config::ModelRunnerType::PIPELINE_MANAGER) {
+        TT_LOG_INFO("[RunnerFactory] Creating SP Pipeline runner (pipeline_manager)");
         return std::make_unique<runners::SpPipelineRunner>(cfg, resultQueue,
                                                            taskQueue);
       }
+#endif
 
       if (cfg.runner_type == config::ModelRunnerType::PREFILL) {
         TT_LOG_INFO("[RunnerFactory] Creating SP Prefill runner");
         return std::make_unique<runners::SpPrefillRunner>(cfg, resultQueue,
                                                           taskQueue);
       }
-#endif
 
       TT_LOG_INFO("[RunnerFactory] Creating LLM runner (mock)");
       return std::make_unique<tt::runners::LLMRunner>(cfg, resultQueue,
