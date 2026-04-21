28702b6269594fb5c55333e982f8dc65361b73ce
Kosta Novokmet <knovokmet@tenstorrent.com>
2026-04-21 10:10:15 +0200

    cpp server - blaze: fix hang detection (#3059)
    * fix hang detection
    
    * format

--- tt-media-server/cpp_server/include/runners/sp_pipeline_runner/blaze_runner.hpp
+++ tt-media-server/cpp_server/include/runners/sp_pipeline_runner/blaze_runner.hpp
@@ -17,6 +17,7 @@
 #include "runners/llm_runner/sequence.hpp"
 #include "runners/llm_runner/task_queue.hpp"
 #include "runners/runner_interface.hpp"
+#include "runners/sp_pipeline_runner/blaze_utils.hpp"
 #include "services/memory_services/async_memory_manager.hpp"
 
 namespace tt::runners {
@@ -55,12 +56,9 @@ class BlazeRunner : public IRunner {
   ipc::IResultQueue* resultQueue;
   tt::runners::llm_engine::ITaskQueue* taskQueue;
   std::unique_ptr<pm::PipelineManager> pipelineManager;
-  std::unordered_map<uint32_t,
-                     std::unique_ptr<tt::runners::llm_engine::Sequence>>
-      running;
+  std::unordered_map<uint32_t, blaze_utils::SlotContext> slotContexts;
   std::atomic<bool> stopped{false};
   std::unique_ptr<tt::services::AsyncMemoryManager> memoryManager;
-
   std::chrono::steady_clock::time_point lastOutputTime;
   std::chrono::milliseconds outputHangTimeout;
 };
