42618a934c04935fb3faee00031bb2627783b11c
Ioannis Pastaltzidis <ipastaltzidis@tenstorrent.com>
2026-04-24 13:20:03 +0300

    update: bump t3k llama-3.1-8b trace region

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -2052,7 +2052,7 @@ llm_templates = [
                 max_context=128 * 1024,
                 default_impl=True,
                 override_tt_config={
-                    "trace_region_size": 83000000,
+                    "trace_region_size": 90000000,
                 },
             ),
             DeviceModelSpec(
