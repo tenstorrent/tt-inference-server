30336f89edfe5d1cd9a844c8622b446bbdf4adfb
Djordje Madic <dmadic@tenstorrent.com>
2026-04-27 14:17:49 +0000

    spec decode true

--- tt-media-server/cpp_server/include/runners/sp_pipeline_runner/blaze_utils.hpp
+++ tt-media-server/cpp_server/include/runners/sp_pipeline_runner/blaze_utils.hpp
@@ -26,7 +26,7 @@ inline pm::GenerationParams makeGenerationParams(
       .max_new_tokens =
           static_cast<uint32_t>(seq.getSamplingParams().max_tokens.value_or(
               static_cast<int>(config::LLMConfig::MAX_INPUT_TOKENS))),
-      .spec_decode = seq.getSamplingParams().fast_mode,
+      .spec_decode = true,
       .ignore_eos = seq.getSamplingParams().ignore_eos,
       .temperature = seq.getSamplingParams().temperature,
       .top_p = seq.getSamplingParams().top_p.value_or(1.0f),
