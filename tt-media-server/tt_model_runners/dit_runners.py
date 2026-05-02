613573ca49562281da7832be981e487f38895941
Veljko Cankovic <vcankovic@tenstorrent.com>
2026-05-02 19:33:18 +0200

    Update dit_runners.py added reliability_mode #3265

--- tt-media-server/tt_model_runners/dit_runners.py
+++ tt-media-server/tt_model_runners/dit_runners.py
@@ -386,6 +386,7 @@ class TTWan22Runner(TTDiTRunner):
                 device_params["fabric_router_config"] = config
             elif is_blackhole() and mesh_shape == (2, 2):                                                                                                                                                                                                                      
                 device_params["fabric_config"] = ttnn.FabricConfig.FABRIC_2D
+                device_params["reliability_mode"] = ttnn.FabricReliabilityMode.RELAXED_INIT 
         return device_params
 
 
