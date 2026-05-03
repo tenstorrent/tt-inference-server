394e0290f643193dc441995d1dd0a83d41c7b0d4
Veljko Cankovic <vcankovic@tenstorrent.com>
2026-05-03 17:16:39 +0200

    Update model_spec.py added hf_overrides in model_spec

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1431,6 +1431,7 @@ llm_templates = [
                 vllm_args={
                     "limit-mm-per-prompt": json.dumps({"image": 1}),
                     "mm_processor_cache_gb": 0,
+                    "hf_overrides": json.dumps({"architectures": ["Mistral3ForConditionalGeneration"]}),
                 },
             ),
         ],
