108a216ace397004dfe52eae078b07db499d9f65
lukezTT <lzhang@tenstorrent.com>
2026-05-04 19:00:51 +0000

    updated max_context to 32*1024

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -2035,7 +2035,7 @@ llm_templates = [
             DeviceModelSpec(
                 device=DeviceTypes.N150,
                 max_concurrency=32,
-                max_context=64 * 1024,
+                max_context=32 * 1024,
                 default_impl=True,
             ),
             DeviceModelSpec(
