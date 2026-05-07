3b5dd7d18e91136306c04795479332f7211e2f2c
Igor Djuric <idjuric@tenstorrent.com>
2026-05-07 12:39:40 +0000

    Untie session & session manager from streaming writers

--- tt-media-server/cpp_server/include/api/llm_controller.hpp
+++ tt-media-server/cpp_server/include/api/llm_controller.hpp
@@ -137,7 +137,7 @@ class LLMController : public drogon::HttpController<LLMController> {
    * ResponseWriter. Common to both streaming and non-streaming code paths.
    */
   static std::function<void(const LLMStreamChunk&, bool)> makeStreamingCallback(
-      std::shared_ptr<ResponseWriter> writer);
+      std::shared_ptr<ResponseWriter> writer, domain::Session* session);
 };
 
 }  // namespace tt::api
