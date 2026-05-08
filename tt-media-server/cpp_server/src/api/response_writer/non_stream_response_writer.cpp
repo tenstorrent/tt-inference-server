e3a5baa0e571d60a755eecf6fe70d3e60ab3bf61
Lana Jovanovic <ljovanovic@tenstorrent.com>
2026-05-08 15:56:50 +0200

    Add V1 Responses Endpoint (#3007)
    * Add V1 Responses Api
    
    * Minor Fixes
    
    * Fix Linter
    
    * Enable Streaming
    
    * Fix Linter
    
    * Fix Rebasing
    
    * Fix
    
    * Add Respones Id

--- tt-media-server/cpp_server/src/api/response_writer/non_stream_response_writer.cpp
+++ tt-media-server/cpp_server/src/api/response_writer/non_stream_response_writer.cpp
@@ -14,15 +14,35 @@ namespace tt::api {
 
 using namespace tt::domain::llm;
 
+namespace {
+
+std::string defaultChatCompletionBuilder(const LLMResponse& response) {
+  return ChatCompletionResponse::fromLLMResponse(response).toJsonString();
+}
+
+}  // namespace
+
 NonStreamResponseWriter::NonStreamResponseWriter(ResponseWriterParams params,
-                                                 HttpCallback httpCallback)
+                                                 HttpCallback httpCallback,
+                                                 ResponseBuilder builder)
     : ResponseWriter(std::move(params)),
-      httpCallback(std::move(httpCallback)) {}
+      httpCallback(std::move(httpCallback)),
+      builder(std::move(builder)) {}
 
 std::shared_ptr<NonStreamResponseWriter> NonStreamResponseWriter::create(
     ResponseWriterParams params, HttpCallback httpCallback) {
-  return std::shared_ptr<NonStreamResponseWriter>(
-      new NonStreamResponseWriter(std::move(params), std::move(httpCallback)));
+  return create(std::move(params), std::move(httpCallback),
+                &defaultChatCompletionBuilder);
+}
+
+std::shared_ptr<NonStreamResponseWriter> NonStreamResponseWriter::create(
+    ResponseWriterParams params, HttpCallback httpCallback,
+    ResponseBuilder builder) {
+  if (!builder) {
+    builder = &defaultChatCompletionBuilder;
+  }
+  return std::shared_ptr<NonStreamResponseWriter>(new NonStreamResponseWriter(
+      std::move(params), std::move(httpCallback), std::move(builder)));
 }
 
 void NonStreamResponseWriter::handleTokenChunk(const LLMStreamChunk& chunk) {
@@ -72,11 +92,9 @@ void NonStreamResponseWriter::finalize() {
     }
   }
 
-  auto chatResponse = ChatCompletionResponse::fromLLMResponse(llmResponse);
-
   auto resp = drogon::HttpResponse::newHttpResponse();
   resp->setContentTypeCode(drogon::CT_APPLICATION_JSON);
-  resp->setBody(chatResponse.toJsonString());
+  resp->setBody(builder(llmResponse));
 
   if (httpCallback) {
     auto cb = std::move(httpCallback);
