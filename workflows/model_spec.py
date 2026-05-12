f36661a3443819001c62100645b8113ebd52266c
lukezTT <lzhang@tenstorrent.com>
2026-05-12 19:22:11 +0000

    bumped up qwen3-32b as well

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1378,7 +1378,7 @@ llm_templates = [
                 max_context=128 * 1024,
                 default_impl=True,
                 override_tt_config={
-                    "trace_region_size": 120000000,
+                    "trace_region_size": 134217728,
                 },
             ),
         ],
