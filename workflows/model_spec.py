e799052562d9604dd27d7087dc7428d88c595ac2
Benjamin Goel <bgoel@tenstorrent.com>
2026-05-15 09:08:36 -0400

    Ben/fix p300x2 trace region (#3571)
    * Increase Llama-70B trace_region_size & set has_builtin_warmup=True
    
    * Increase Qwen3-32B trace_region_size & set has_builtin_warmup=True
    
    * Bump trace_region_sizes again
    
    * Bump again
    
    * Bump again
    
    * Bump 70B
    
    * Final bump

--- workflows/model_spec.py
+++ workflows/model_spec.py
@@ -1388,7 +1388,7 @@ llm_templates = [
                 max_context=128 * 1024,
                 default_impl=True,
                 override_tt_config={
-                    "trace_region_size": 256 * 1024 * 1024,
+                    "trace_region_size": 384 * 1024 * 1024,
                 },
             ),
         ],
