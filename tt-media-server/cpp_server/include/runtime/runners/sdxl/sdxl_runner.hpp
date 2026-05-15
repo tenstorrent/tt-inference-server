38ab4991b2cebf2adebbed58f52fc485c86f3134
Ljubica Muravljov <lmuravljov@tenstorrent.com>
2026-05-15 13:55:52 +0000

    remove sdxl runner

--- tt-media-server/cpp_server/include/runtime/runners/sdxl/sdxl_runner.hpp
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: © 2026 Tenstorrent USA, Inc.
-
-#pragma once
-
-#include "runtime/runners/sdxl/sdxl_base_runner.hpp"
-#include "runtime/runners/sdxl/sdxl_edit_runner.hpp"
-#include "runtime/runners/sdxl/sdxl_generate_runner.hpp"
-#include "runtime/runners/sdxl/sdxl_image_to_image_runner.hpp"
