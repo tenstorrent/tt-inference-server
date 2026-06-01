619112c1427de84d6dcc99eee44e6bf34f0e7724
Kosta Novokmet <knovokmet@tenstorrent.com>
2026-06-01 08:19:08 +0000

    init

--- tt-media-server/cpp_server/include/runtime/runners/blaze_runner/blaze_utils.hpp
+++ tt-media-server/cpp_server/include/runtime/runners/blaze_runner/blaze_utils.hpp
@@ -134,7 +134,7 @@ inline pl::PipelineConfig makePipelineConfig(
           .h2d_socket_id = tt::config::blazeSocketDescriptorPrefix() + "_h2d",
           .d2h_socket_id = tt::config::blazeSocketDescriptorPrefix() + "_d2h",
           .connect_timeout_ms = tt::config::pmConnectTimeoutMs(),
-          .wire_format = tt_llm_engine::pipeline::WireFormat::LOOPBACK};
+          .wire_format = tt_llm_engine::pipeline::WireFormat::DEEPSEEK};
     case tt::config::ModelRunnerType::MOCK_PIPELINE:
       return pl::PipelineSimulatorConfig{
           .num_stages = 64,
