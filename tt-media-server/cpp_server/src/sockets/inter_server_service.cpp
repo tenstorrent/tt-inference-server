249728f120781016fa131e7b91abd6c09e1bb91e
Djordje Madic <dmadic@tenstorrent.com>
2026-06-04 20:19:17 +0000

    Format

--- tt-media-server/cpp_server/src/sockets/inter_server_service.cpp
+++ tt-media-server/cpp_server/src/sockets/inter_server_service.cpp
@@ -291,8 +291,7 @@ void InterServerService::setupMessageHandlers() {
       });
 
   socket_manager_.registerHandler<PrefillRegistrationMessage>(
-      tags::PREFILL_REGISTRATION, [](const PrefillRegistrationMessage& msg) {
-      });
+      tags::PREFILL_REGISTRATION, [](const PrefillRegistrationMessage& msg) {});
 
   // Handle incoming prefill results
   socket_manager_.registerHandler<PrefillResultMessage>(
