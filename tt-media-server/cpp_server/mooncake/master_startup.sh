100a12b7ea4ecee3369dc71068c35f70124de461
Filip Ivanovic <fivanovic@tenstorrent.com>
2026-06-04 07:57:05 +0000

    feat: simple mooncake setup

---
+++ tt-media-server/cpp_server/mooncake/master_startup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Start the Mooncake master
+mooncake_master \
+  --enable_http_metadata_server=true \
+  --http_metadata_server_host=0.0.0.0 \
+  --http_metadata_server_port=8080 \
+  --port=50051
