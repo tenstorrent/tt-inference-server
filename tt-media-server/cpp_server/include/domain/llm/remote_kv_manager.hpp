2465130758c11323cb1249d4ce8196aebc5b102b
Djordje Madic <dmadic@tenstorrent.com>
2026-06-16 09:39:08 +0000

    Change log to trigger build

---
+++ tt-media-server/cpp_server/include/domain/llm/remote_kv_manager.hpp
@@ -0,0 +1,15 @@
+// Abstract interface for remote KV manager
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace tt::domain::llm {
+
+class RemoteKvManager {
+  public:
+    virtual ~RemoteKvManager() = default;
+
+    
+}
