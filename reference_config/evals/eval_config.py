72015d4a59c1d69b1512c795450e496df226bdd2
mvasiljevic <mvasiljevic@tenstorrent.com>
2026-09-17 14:52:36 +0000

    Run scikit-learn__scikit-learn-14629 in SWE CI

--- reference_config/evals/eval_config.py
+++ reference_config/evals/eval_config.py
@@ -1499,7 +1499,7 @@ _eval_config_list = [
                     },
                     instance_ids_map={
                         EvalLimitMode.CI_NIGHTLY: [
-                            "django__django-11299",
+                            "scikit-learn__scikit-learn-14629",
                         ],
                     },
                 ),
